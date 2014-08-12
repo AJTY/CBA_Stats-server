2b4a35020a23193c6b886d3da07d1f410f7f20fc
Tomas Sykora, jr <jump@syky.cz>
2014-08-12 15:31:09 +0200

    schema added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :ligas
   resources :repre_soupiskas
+  resources :liga_hracis
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
